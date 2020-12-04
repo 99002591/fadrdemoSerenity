71d73958287ac01fc392688dc7bcf4b47905516b
99002591 <amit.das@ltts.com>
2020-12-04 18:03:12 +0530

    stepDefinations ready

--- src/test/java/fadrdemoSerenity/cucumber/StepDefinitions/loginAPI.java
+++ src/test/java/fadrdemoSerenity/cucumber/StepDefinitions/loginAPI.java
@@ -14,25 +14,27 @@ public class loginAPI {
 	
 	@Given("^Prepare \"([^\"]*)\" for Login API$")
 	public void prepare_something_for_login_api(String requestbody) throws Exception {
+		login_actions.prepare_request_body(requestbody);
 	}
 
 	@When("^POST request on Login API$")
 	public void post_request_on_login_api() {
-
+		login_actions.post_request_to_endpoint();
 	}
 
 	@Then("^Status code returned is \"([^\"]*)\"$")
 	public void status_code_returned_is_something(int statuscode) {
-		
+		login_actions.validate_the_statuscode();
+		login_actions.validate_the_statusline();
 	}
 
 	@And("^Error code returned is \"([^\"]*)\"$")
 	public void error_code_returned_is_something(String errorcode) {
-	
+		login_actions.validate_the_errorcode();
 	}
 
 	@And("^Response body returned is \"([^\"]*)\"$")
 	public void response_body_returned_is_something(String errordescription) {
-
+		login_actions.validate_the_errordescription();
 	}
 }
--- src/test/java/fadrdemoSerenity/cucumber/StepsSerenity/loginAPI_steps.java
+++ src/test/java/fadrdemoSerenity/cucumber/StepsSerenity/loginAPI_steps.java
@@ -1,5 +1,32 @@
 package fadrdemoSerenity.cucumber.StepsSerenity;
 
+import io.restassured.response.ValidatableResponse;
+
 public class loginAPI_steps {
 
+	static ValidatableResponse response_all;
+
+	public void prepare_request_body(String response) {
+
+	}
+
+	public void post_request_to_endpoint() {
+
+	}
+
+	public void validate_the_statuscode() {
+
+	}
+
+	public void validate_the_statusline() {
+
+	}
+	
+	public void validate_the_errorcode() {
+		
+	}
+	
+	public void validate_the_errordescription() {
+		
+	}
 }
