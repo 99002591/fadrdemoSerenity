09b991a2bd22f9602731d3bf0dfb691df1320fc1
99002591 <amit.das@ltts.com>
2020-12-04 18:09:10 +0530

    Repeated steps annotated

--- src/test/java/fadrdemoSerenity/cucumber/StepsSerenity/loginAPI_steps.java
+++ src/test/java/fadrdemoSerenity/cucumber/StepsSerenity/loginAPI_steps.java
@@ -1,32 +1,39 @@
 package fadrdemoSerenity.cucumber.StepsSerenity;
 
 import io.restassured.response.ValidatableResponse;
+import net.thucydides.core.annotations.Step;
 
 public class loginAPI_steps {
 
 	static ValidatableResponse response_all;
 
+	@Step
 	public void prepare_request_body(String response) {
 
 	}
 
+	@Step
 	public void post_request_to_endpoint() {
 
 	}
 
+	@Step
 	public void validate_the_statuscode() {
 
 	}
 
+	@Step
 	public void validate_the_statusline() {
 
 	}
-	
+
+	@Step
 	public void validate_the_errorcode() {
-		
+
 	}
-	
+
+	@Step
 	public void validate_the_errordescription() {
-		
+
 	}
 }
