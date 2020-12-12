8fbe58414312f4e10c94a15b11fc0654e904964d
99002591 <amit.das@ltts.com>
2020-12-12 15:22:29 +0530

    Program runs from terminal, command = "mvn clean verify serenity:aggregate"

---
+++ src/test/java/runner/RunnerTest.java
@@ -0,0 +1,18 @@
+package runner;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import net.serenitybdd.junit.runners.SerenityRunner;
+
+@RunWith(CucumberWithSerenity.class)
+
+
+@CucumberOptions(
+        features = "src/test/resources/features/",
+        glue= "StepDefinitions",
+        monochrome = true,
+        plugin = "html:target/automationreport.html")
+public class RunnerTest {
+}
