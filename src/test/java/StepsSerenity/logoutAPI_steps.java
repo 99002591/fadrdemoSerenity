22a8cd6b774ae3a2bf142c4426c22d9be5467de2
99002591 <amit.das@ltts.com>
2020-12-14 11:24:57 +0530

    Handled case for expired token

--- src/test/java/StepsSerenity/logoutAPI_steps.java
+++ src/test/java/StepsSerenity/logoutAPI_steps.java
@@ -6,23 +6,30 @@ import io.restassured.response.ValidatableResponse;
 import net.serenitybdd.rest.SerenityRest;
 import net.thucydides.core.annotations.Step;
 import utils.commonMethods;
+import utils.fileReader;
 
 public class logoutAPI_steps {
 	static custRegAPI_steps custRegAPI_steps_object = new custRegAPI_steps();
 	static hooks hooks_object = new hooks();
 	static commonMethods commonMethods_object = new commonMethods();
+	static fileReader fileReader_object = new fileReader();
 	static ValidatableResponse response_all;
 	static String auth, id;
-	static String auth_expired = "sgfwqujgjaslfgsauilfgslaufgsuair934234792305bjxzkvba";
-	static String auth_invalid = auth + "ashdsjkfhsk2371249jdfsdfdsfdsfdsfsd3w43rf52353523kcns";
+	static String auth_expired;
+	static String auth_invalid = "ashdsjkfhsk2371249jdfsdfdsfdsfdsfsd3w43rf52353523kcns";
 	static String auth_blank = "";
 	static String id_blank = "";
-	
+
 	@Step
 	public void generate_token_and_id() {
 		response_all = custRegAPI_steps_object.get_token();
 		auth = response_all.extract().response().jsonPath().getString("data.token");
 		id = response_all.extract().response().jsonPath().getString("data.id");
+		try {
+			auth_expired = fileReader_object.readfile("expired_token");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Step
@@ -38,7 +45,7 @@ public class logoutAPI_steps {
 			if (useridtype.equals("valid")) {
 				response_all = commonMethods_object.delete_request_valid_userID(auth_invalid, id);
 			} else if (useridtype.equals("invalid")) {
-				response_all = commonMethods_object.delete_request_invalid_userID(auth_invalid,id+"uhfishfaih213");
+				response_all = commonMethods_object.delete_request_invalid_userID(auth_invalid, id + "uhfishfaih213");
 			} else if (useridtype.equals("blank")) {
 				response_all = commonMethods_object.delete_request_blank_userID(auth_invalid, id_blank);
 			}
@@ -46,7 +53,7 @@ public class logoutAPI_steps {
 			if (useridtype.equals("valid")) {
 				response_all = commonMethods_object.delete_request_valid_userID(auth_invalid, id);
 			} else if (useridtype.equals("invalid")) {
-				response_all = commonMethods_object.delete_request_invalid_userID(auth_invalid,id+"uhfishfaih213");
+				response_all = commonMethods_object.delete_request_invalid_userID(auth_invalid, id + "uhfishfaih213");
 			} else if (useridtype.equals("blank")) {
 				response_all = commonMethods_object.delete_request_blank_userID(auth_invalid, id_blank);
 			}
@@ -54,7 +61,7 @@ public class logoutAPI_steps {
 			if (useridtype.equals("valid")) {
 				response_all = commonMethods_object.delete_request_valid_userID(auth_expired, id);
 			} else if (useridtype.equals("invalid")) {
-				response_all = commonMethods_object.delete_request_invalid_userID(auth_expired,id+"uhfishfaih213");
+				response_all = commonMethods_object.delete_request_invalid_userID(auth_expired, id + "uhfishfaih213");
 			} else if (useridtype.equals("blank")) {
 				response_all = commonMethods_object.delete_request_blank_userID(auth_expired, id_blank);
 			}
@@ -62,11 +69,11 @@ public class logoutAPI_steps {
 			if (useridtype.equals("valid")) {
 				response_all = commonMethods_object.delete_request_valid_userID(auth, id);
 			} else if (useridtype.equals("invalid")) {
-				response_all = commonMethods_object.delete_request_invalid_userID(auth,id+"uhfishfaih213");
+				response_all = commonMethods_object.delete_request_invalid_userID(auth, id + "uhfishfaih213");
 			} else if (useridtype.equals("blank")) {
 				response_all = commonMethods_object.delete_request_blank_userID(auth, id_blank);
 			}
 		}
-	return response_all;
+		return response_all;
 	}
 }
---
+++ src/test/java/utils/fileReader.java
@@ -0,0 +1,20 @@
+package utils;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class fileReader {
+public String content = null,line;
+	
+	public String readfile(String fileName) throws Exception {
+		FileReader f_read = new FileReader("D:\\Eclipse_Workspace\\fadrdemoSerenity\\src\\test\\resources\\Tokens\\"+fileName);
+		BufferedReader b_read = new BufferedReader(f_read);
+		 while ((line = b_read.readLine()) != null) {
+			 content += line;
+		 }
+		 b_read.close();
+		 f_read.close();
+		 content = content.replaceAll("null", "");
+		return content;
+	}
+}
